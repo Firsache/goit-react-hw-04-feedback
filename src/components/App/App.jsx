07e3747215f95149648943fa0efbae18fda5d6bf
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-17 11:38:17 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,19 +11,9 @@ export function App() {
   let total = good + bad + neutral;
 
   const onLeaveFeedback = option => {
-    switch (option) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-      default:
-        return;
-    }
+    if (option === 'good') setGood(prevState => prevState + 1);
+    if (option === 'neutral') setNeutral(prevState => prevState + 1);
+    if (option === 'bad') setBad(prevState => prevState + 1);
   };
 
   const countPositiveFeedbackPercentage = () => {
